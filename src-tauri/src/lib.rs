500b5d4d31ca6bf9ff1ee8e16a12761906913f06
R894 <R894@users.noreply.github.com>
2024-12-07 17:57:16 +0200

    add limbus patching

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,102 +1,156 @@
-use reqwest::blocking::get;
-use std::fs::File;
-use std::fs::{self, create_dir_all};
-use std::io::{self};
+use futures::stream::StreamExt;
+use pelite::FileMap;
+use reqwest::Client;
+use std::fs;
 use std::path::Path;
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
 use zip::read::ZipArchive;
 
 #[tauri::command]
-fn clone_folder_to_game(src_path: String) -> Result<(), String> {
-    let src = Path::new(&src_path);
-    let dest = Path::new("./game");
-
-    if !dest.exists() {
-        fs::create_dir_all(dest)
-            .map_err(|e| format!("Failed to create destination directory: {}", e))?;
-    }
-
-    if src.is_dir() {
-        for entry in
-            fs::read_dir(src).map_err(|e| format!("Failed to read source directory: {}", e))?
-        {
-            let entry = entry.map_err(|e| format!("Error reading entry: {}", e))?;
-            let file_name = entry.file_name();
-            let destination = dest.join(file_name);
-
-            if entry.path().is_file() {
-                fs::copy(entry.path(), destination)
-                    .map_err(|e| format!("Failed to copy file: {}", e))?;
-            } else if entry.path().is_dir() {
-                fs::create_dir_all(&destination)
-                    .map_err(|e| format!("Failed to create subdirectory: {}", e))?;
-                clone_folder_to_game(entry.path().to_str().unwrap().to_string())?;
-            }
-        }
-        Ok(())
-    } else {
-        Err("Source path is not a valid directory".into())
-    }
+async fn patch_limbus(src_path: String) -> Result<(), String> {
+    let limbus_exe = src_path + "/LimbusCompany.exe";
+    patch_limbus_exe(limbus_exe)?;
+    Ok(())
 }
 
 #[tauri::command]
-fn download_and_extract_bepinex() {
+async fn download_and_extract_bepinex() -> Result<(), String> {
     let url = "https://builds.bepinex.dev/projects/bepinex_be/577/BepInEx_UnityIL2CPP_x64_ec79ad0_6.0.0-be.577.zip";
     let zip_path = "BepInEx_UnityIL2CPP_x64_ec79ad0_6.0.0-be.577.zip";
     let extract_to = "game";
 
-    if let Err(e) = download_file(url, zip_path) {
-        eprintln!("Failed to download the file: {}", e);
-        return;
-    }
-    println!("Downloaded ZIP file successfully.");
-
-    if let Err(e) = unzip_file(zip_path, extract_to) {
-        eprintln!("Failed to unzip the file: {}", e);
-        return;
-    }
-    println!("Unzipped the file successfully.");
+    download_file(url, zip_path)
+        .await
+        .map_err(|e| format!("Error downloading file: {}", e))?;
+    unzip_file(zip_path, extract_to).map_err(|e| format!("Error unzipping file: {}", e))?;
+    Ok(())
 }
 
 #[tauri::command]
-fn download_and_install_lethe() {
-    let url = ""; // TODO: Add lethe url
-    let destination = "game/bepinex/plugins";
+async fn download_and_install_lethe() -> Result<(), String> {
+    let url = "https://lethelc.site/Lethe.dll";
+    let destination = "game/bepinex/plugins/Lethe.dll";
 
-    if let Err(e) = download_file(url, destination) {
-        eprintln!("Failed to download the file: {}", e);
-        return;
-    }
+    download_file(url, destination)
+        .await
+        .map_err(|e| format!("Failed to download the file: {}", e))?;
 
-    println!("Downloaded the file successfully.");
+    Ok(())
 }
 
-fn download_file(url: &str, destination: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let response = get(url)?;
-    let mut file = File::create(destination)?;
-    io::copy(&mut response.bytes()?.as_ref(), &mut file)?;
+async fn download_file(url: &str, destination: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+    let response = client.get(url).send().await?;
+
+    if !response.status().is_success() {
+        return Err(format!("Failed to download file: {}", response.status()).into());
+    }
+
+    let mut file = File::create(destination).await?;
+    let mut content = response.bytes_stream();
+
+    while let Some(chunk) = content.next().await {
+        let chunk = chunk?;
+        file.write_all(&chunk).await?;
+    }
+
     Ok(())
 }
 
 fn unzip_file(zip_path: &str, extract_to: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let file = File::open(zip_path)?;
+    let file = std::fs::File::open(zip_path)?;
     let mut archive = ZipArchive::new(file)?;
 
-    create_dir_all(extract_to)?;
+    std::fs::create_dir_all(extract_to)?;
     for i in 0..archive.len() {
         let mut file = archive.by_index(i)?;
         let outpath = Path::new(extract_to).join(file.name());
 
         if file.name().ends_with('/') {
-            create_dir_all(outpath)?;
+            std::fs::create_dir_all(outpath)?;
         } else {
             if let Some(parent) = outpath.parent() {
-                create_dir_all(parent)?;
+                std::fs::create_dir_all(parent)?;
             }
-            let mut outfile = File::create(outpath)?;
-            io::copy(&mut file, &mut outfile)?;
+            let mut outfile = std::fs::File::create(outpath)?;
+            std::io::copy(&mut file, &mut outfile)?;
         }
     }
 
+    Ok(())
+}
+
+#[tauri::command]
+async fn clone_folder_to_game(src_path: String) -> Result<(), String> {
+    let src = Path::new(&src_path);
+    let dest = Path::new("./game");
+
+    copy_dir_all(src, dest).map_err(|e| format!("Failed to clone folder: {}", e))?;
+    Ok(())
+}
+
+fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> std::io::Result<()> {
+    std::fs::create_dir_all(&dst)?;
+    for entry in std::fs::read_dir(src)? {
+        let entry = entry?;
+        let ty = entry.file_type()?;
+        if ty.is_dir() {
+            copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        } else {
+            std::fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        }
+    }
+    Ok(())
+}
+
+#[tauri::command]
+fn add_distribute_files() -> Result<(), String> {
+    let source_dir = Path::new("./distribute");
+    let target_dir = Path::new("./game");
+
+    if !target_dir.exists() {
+        std::fs::create_dir(target_dir)
+            .map_err(|e| format!("Failed to create target directory: {}", e))?;
+    }
+
+    copy_directory_recursively(source_dir, target_dir)
+        .map_err(|err| format!("Failed to copy directory recursively: {}", err))?;
+    println!(
+        "Files copied from {} to {}",
+        source_dir.display(),
+        target_dir.display()
+    );
+    Ok(())
+}
+
+fn copy_directory_recursively(source: &Path, destination: &Path) -> std::io::Result<()> {
+    if !destination.exists() {
+        std::fs::create_dir(destination)?;
+    }
+    for entry in std::fs::read_dir(source)? {
+        let entry = entry?;
+        let dest_path = destination.join(entry.file_name());
+        if entry.path().is_dir() {
+            copy_directory_recursively(&entry.path(), &dest_path)?;
+        } else {
+            std::fs::copy(entry.path(), &dest_path)?;
+        }
+    }
+    Ok(())
+}
+
+fn patch_limbus_exe(exe_path: String) -> Result<(), String> {
+    let path = Path::new(&exe_path);
+    let map = FileMap::open(path).map_err(|e| format!("Failed to open file: {}", e))?;
+
+    let new_file = steamnvke::drm::strip_drm_from_exe(map.as_ref())
+        .map_err(|e| format!("Failed to strip DRM: {}", e))?;
+
+    fs::write("./game/LimbusCompany.exe", new_file)
+        .map_err(|e| format!("Failed to write LimbusCompany file: {}", e))?;
+
+    println!("Successfully patched and saved LimbusCompany.exe.");
     Ok(())
 }
 
@@ -109,7 +163,9 @@ pub fn run() {
         .invoke_handler(tauri::generate_handler![
             clone_folder_to_game,
             download_and_extract_bepinex,
-            download_and_install_lethe
+            download_and_install_lethe,
+            patch_limbus,
+            add_distribute_files
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");

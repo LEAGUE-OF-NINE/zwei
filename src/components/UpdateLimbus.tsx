e31dabe07c45cc7372b40410485197aded357152
R894 <R894@users.noreply.github.com>
2024-12-08 14:21:15 +0200

    add visual feedback

--- src/components/UpdateLimbus.tsx
+++ src/components/UpdateLimbus.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { open } from "@tauri-apps/plugin-dialog";
 import { invoke } from "@tauri-apps/api/core";
+import Modal from "./Modal";
+
 const UpdateLimbus = () => {
   const [path, setPath] = useState<string | null>(null);
   const [isModalOpen, setModalOpen] = useState(false);
@@ -17,13 +19,13 @@ const UpdateLimbus = () => {
   async function updateLimbus() {
     if (path) {
       try {
-        setInfo("Cloning game folder....");
+        setInfo("Cloning game folder...");
         await invoke("clone_folder_to_game", { srcPath: path });
-        setInfo("Downloading and extracting bepinex....");
+        setInfo("Downloading and extracting bepinex...");
         await invoke("download_and_extract_bepinex");
-        setInfo("Downloading and extracting lethe....");
+        setInfo("Downloading and extracting lethe...");
         await invoke("download_and_install_lethe");
-        setInfo("Patching limbus....");
+        setInfo("Patching limbus...");
         await invoke("patch_limbus", { srcPath: path });
       } catch (error) {
         console.error("Failed to update limbus:", error);
@@ -45,37 +47,27 @@ const UpdateLimbus = () => {
         Update/Install Limbus
       </button>
 
-      {isModalOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-          <div className="modal-box">
-            <h2 className="text-lg font-bold mb-4">Update/Install Limbus</h2>
-            <input
-              type="text"
-              className="input w-full mb-4 bg-base-200 hover:cursor-pointer border-disabled"
-              onClick={selectFolder}
-              placeholder={path || "Select Limbus Folder..."}
-              readOnly
-            />
-            {info && <p>{info}</p>}
-            <div className="flex justify-end space-x-2">
-              <button
-                className="btn btn-secondary"
-                onClick={() => setModalOpen(false)}
-              >
-                Cancel
-              </button>
-              <button
-                className="btn btn-primary"
-                onClick={() => {
-                  updateLimbus();
-                }}
-              >
-                Update Limbus
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
+      <Modal
+        isOpen={isModalOpen}
+        title="Update/Install Limbus"
+        onClose={() => setModalOpen(false)}
+        actions={
+          <>
+            <button className="btn btn-primary" onClick={updateLimbus} disabled={info != null}>
+              Update Limbus
+            </button>
+          </>
+        }
+      >
+        <input
+          type="text"
+          className="input w-full mb-4 bg-base-200 hover:cursor-pointer border-disabled"
+          onClick={selectFolder}
+          placeholder={path || "Select Limbus Folder..."}
+          readOnly
+        />
+        {info && <p>{info}</p>}
+      </Modal>
     </div>
   );
 };

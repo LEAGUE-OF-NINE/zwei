92a7697fdfbf6583a0c1a7c75ea441fb3cc4b5b0
R894 <R894@users.noreply.github.com>
2024-12-12 19:07:48 +0200

    add launch button feedback

--- src/components/StartLethe.tsx
+++ src/components/StartLethe.tsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
 import { useErrorHandler } from "../context/useErrorHandler";
 import { open } from "@tauri-apps/plugin-shell";
+import { listen } from "@tauri-apps/api/event";
 
 const StartLethe = () => {
   const handleError = useErrorHandler();
+  const [message, setMessage] = useState<string>("");
+
+  useEffect(() => {
+    const unlisten = listen<string>("launch-status", (event) => {
+      console.log(`Received event: ${event.payload}`);
+      setMessage(event.payload || "");
+    });
+
+    return () => {
+      unlisten.then((unsub) => unsub());
+    };
+  }, []);
+
   async function startLethe() {
     try {
       await open("https://lethelc.site/zwei");
@@ -11,13 +26,16 @@ const StartLethe = () => {
       handleError(error);
     }
   }
+
   return (
-    <button
-      className="btn btn-primary w-full max-w-sm text-xl font-semibold rounded-tr-none rounded-br-none"
-      onClick={startLethe}
-    >
-      Launch
-    </button>
+    <>
+      <button
+        className="btn btn-primary w-full max-w-sm text-xl font-semibold rounded-tr-none rounded-br-none"
+        onClick={startLethe}
+      >
+        {message ? message : "Launch"}
+      </button>
+    </>
   );
 };
 
